331f8b5ac0041e7ba8f01c2b85389628703deace
sp9pw <142604753+Pawel-sp9pw@users.noreply.github.com>
2026-09-07 14:53:35 +0200

    Add manual approval auth plugin metadata

---
+++ moodle/auth/manualapproval/version.php
@@ -0,0 +1,11 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'auth_manualapproval';
+$plugin->version = 2026090700;
+$plugin->requires = 2026042000;
+$plugin->supported = [502, 502];
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '1.0.0';
