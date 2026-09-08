dd31589879c1c849828291aaf48bda65da4dc490
sp9pw <142604753+Pawel-sp9pw@users.noreply.github.com>
2026-09-08 14:29:47 +0200

    Add FUB Moodle theme version

---
+++ moodle/theme/fub/version.php
@@ -0,0 +1,10 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'theme_fub';
+$plugin->version = 2026090800;
+$plugin->requires = 2026042000;
+$plugin->supported = [502, 502];
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '1.0.0';
