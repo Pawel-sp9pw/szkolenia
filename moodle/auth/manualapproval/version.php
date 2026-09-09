855dbfa1b70c8788e239c7ab04224d05247ffdf1
sp9pw <142604753+Pawel-sp9pw@users.noreply.github.com>
2026-09-09 08:33:20 +0200

    Bump manual approval plugin after admin page fix

--- moodle/auth/manualapproval/version.php
+++ moodle/auth/manualapproval/version.php
@@ -4,8 +4,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'auth_manualapproval';
-$plugin->version = 2026090900;
+$plugin->version = 2026090901;
 $plugin->requires = 2026042000;
 $plugin->supported = [502, 502];
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.0.1';
+$plugin->release = '1.0.2';
