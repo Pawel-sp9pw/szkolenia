2bf3e0bb9709b2ee6d7e7779ceb7746c4111b727
sp9pw <142604753+Pawel-sp9pw@users.noreply.github.com>
2026-09-15 09:05:32 +0200

    Bump FUB theme for student training mode

--- moodle/theme/fub/version.php
+++ moodle/theme/fub/version.php
@@ -3,8 +3,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_fub';
-$plugin->version = 2026091500;
+$plugin->version = 2026091501;
 $plugin->requires = 2026042000;
 $plugin->supported = [502, 502];
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.0.1';
+$plugin->release = '1.0.2';
