52c0dfd1fd1752ce9334ccb9c3d4e5f7ae931e98
sp9pw <142604753+Pawel-sp9pw@users.noreply.github.com>
2026-09-15 08:58:29 +0200

    Bump auth_manualapproval for cohort membership editor

--- moodle/auth/manualapproval/version.php
+++ moodle/auth/manualapproval/version.php
@@ -4,8 +4,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'auth_manualapproval';
-$plugin->version = 2026091500;
+$plugin->version = 2026091501;
 $plugin->requires = 2026042000;
 $plugin->supported = [502, 502];
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.1.0';
+$plugin->release = '1.1.1';
